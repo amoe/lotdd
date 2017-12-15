94afa247706d8dfa682592cc2b2fda90f84d9003
David Banks <amoebae@gmail.com>
2017-12-15 12:44:44 +0000

    fixing the zero pad algorithm

---
+++ src/soundex.hh
@@ -0,0 +1,24 @@
+#ifndef SOUNDEX_HH
+#define SOUNDEX_HH
+
+#include <string>
+
+class Soundex {
+public:
+    std::string encode(const std::string& word) const {
+        auto encoded = word.substr(0, 1);
+        if (word.length() > 1)
+            encoded += "1";
+
+        return zeroPad(encoded);
+    }
+private:
+    std::string zeroPad(const std::string& word) const {
+        auto zerosNeeded = 4 - word.length();
+        
+        return word + std::string(zerosNeeded, '0');
+    }
+};
+
+
+#endif // SOUNDEX_HH
