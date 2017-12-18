e2947907fd81647ed9cb717a2bfd2feb89f177c0
David Banks <amoebae@gmail.com>
2017-12-18 16:51:39 +0000

    refactor soundex code length to class constant

--- src/soundex.hh
+++ src/soundex.hh
@@ -4,19 +4,28 @@
 #include <string>
 
 class Soundex {
+    static const size_t MAX_CODE_LENGTH{4};
+
 public:
     std::string encode(const std::string& word) const {
-        auto encoded = word.substr(0, 1);
-        if (word.length() > 1)
-            encoded += "1";
-
-        return zeroPad(encoded);
+        return zeroPad(head(word) + encodedDigits(word));
     }
 private:
     std::string zeroPad(const std::string& word) const {
-        auto zerosNeeded = 4 - word.length();
+        auto zerosNeeded = MAX_CODE_LENGTH - word.length();
         
         return word + std::string(zerosNeeded, '0');
+    }
+
+    std::string head(const std::string& word) const {
+        return word.substr(0, 1);
+    }
+
+    std::string encodedDigits(const std::string& word) const {
+        if (word.length() > 1)
+            return "1";
+        
+        return "";
     }
 };
 
