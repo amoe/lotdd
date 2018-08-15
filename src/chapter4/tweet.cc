a74f29e23476e0dd5686176137a8a424d5f0f0ca
David Banks <amoebae@gmail.com>
2018-08-15 22:38:25 +0100

    code to do validation on the tweet username

--- src/chapter4/tweet.cc
+++ src/chapter4/tweet.cc
@@ -1,11 +1,22 @@
+#include <stdexcept>
 #include "tweet.hh"
+
+using std::runtime_error;
+
+bool isValid(const string& user) {
+    return user.at(0) == '@';
+}
 
 Tweet::Tweet() {
 }
 
 
 Tweet::Tweet(const string& body, const string& user)  : body(body), user(user) {
+    if (!isValid(user)) {
+        throw std::runtime_error("invalid user");
+    }
 }
+
 
 const string Tweet::getBody() {
     return body;
