19ee71baebd7326b9d99605e1509aa04ccb93857
David Banks <amoebae@gmail.com>
2018-08-25 16:47:53 +0100

    implemented and passing

--- src/chapter5/place_description_service.cc
+++ src/chapter5/place_description_service.cc
@@ -1,8 +1,12 @@
 #include "place_description_service.hh"
 #include "address_extractor.hh"
 
+using std::to_string;
+
 string PlaceDescriptionService::summaryDescription(double latitude, double longitude) {
-    string getRequestUrl = "";
+    string getRequestUrl
+        = "lat=" + to_string(latitude) + "&lon=" + to_string(longitude);
+
     auto jsonResponse = http->get(getRequestUrl);
     
     AddressExtractor extractor;
