aefe9423a1e06cc2a611e76cd6694f8b0052437f
David Banks <amoebae@gmail.com>
2022-10-06 05:35:45 +0100

    modify to use purchaseHelper method

--- src/chapter6/example6/portfolio_test.cc
+++ src/chapter6/example6/portfolio_test.cc
@@ -13,18 +13,31 @@ public:
     static const string stockSymbol1;
     static const string stockSymbol2;
     Portfolio portfolio;
+
+    void purchaseHelper(
+        const string& symbol,
+        unsigned int shareCount,
+        const date& transactionDate=Portfolio::FIXED_PURCHASE_DATE
+    );
 };
 
 const string PortfolioTest::stockSymbol1{"IBM"};
 const string PortfolioTest::stockSymbol2{"SSNLF"};    // Samsung
 
+void PortfolioTest::purchaseHelper(
+    const string& symbol, unsigned int shareCount, const date& transactionDate
+) {
+    portfolio.purchase(symbol, shareCount, transactionDate);
+}
+    
 
-TEST_F(PortfolioTest, answersZeroForShareCountOfUnpurchasedSymbol) {
+
+TEST_F(PortfolioTest, answersZeroForShareCountOfUnpurchaseHelperdSymbol) {
     ASSERT_THAT(portfolio.shareCount("AAPL"), Eq(0u));
 }
 
-TEST_F(PortfolioTest, isNotEmptyAfterPurchase) {
-    portfolio.purchase(stockSymbol1, 1u);
+TEST_F(PortfolioTest, isNotEmptyAfterPurchaseHelper) {
+    purchaseHelper(stockSymbol1, 1u);
     ASSERT_FALSE(portfolio.isEmpty());
 }
 
@@ -32,36 +45,36 @@ TEST_F(PortfolioTest, isEmptyWhenCreated) {
     ASSERT_TRUE(portfolio.isEmpty());
 }
 
-TEST_F(PortfolioTest, answersShareCountForPurchasedSymbol) {
-    portfolio.purchase(stockSymbol1, 2u);
+TEST_F(PortfolioTest, answersShareCountForPurchaseHelperdSymbol) {
+    purchaseHelper(stockSymbol1, 2u);
     ASSERT_THAT(portfolio.shareCount(stockSymbol1), Eq(2u));
 }
 
 
-TEST_F(PortfolioTest, throwsOnPurchaseOfZeroShares) {
-    ASSERT_THROW(portfolio.purchase(stockSymbol1, 0u), InvalidPurchaseException);
+TEST_F(PortfolioTest, throwsOnPurchaseHelperOfZeroShares) {
+    ASSERT_THROW(purchaseHelper(stockSymbol1, 0u), InvalidPurchaseException);
 }
 
 TEST_F(PortfolioTest, answersShareCountForAppropriateSymbol) {
-    portfolio.purchase("AAPL", 2u);
-    portfolio.purchase("IBM", 1u);
+    purchaseHelper("AAPL", 2u);
+    purchaseHelper("IBM", 1u);
     ASSERT_THAT(portfolio.shareCount("AAPL"), Eq(2u));
 }
 
-TEST_F(PortfolioTest, shareCountReflectsAccumulatedPurchasesOfSameSymbol) {
-    portfolio.purchase(stockSymbol1, 1u);
-    portfolio.purchase(stockSymbol1, 2u);
+TEST_F(PortfolioTest, shareCountReflectsAccumulatedPurchaseHelpersOfSameSymbol) {
+    purchaseHelper(stockSymbol1, 1u);
+    purchaseHelper(stockSymbol1, 2u);
     ASSERT_THAT(portfolio.shareCount(stockSymbol1), Eq(3u));
 }
 
 TEST_F(PortfolioTest, reducesShareCountOfSymbolOnSell) {
-    portfolio.purchase(stockSymbol1, 1u);
+    purchaseHelper(stockSymbol1, 1u);
     portfolio.sell(stockSymbol1, 1u);
     ASSERT_THAT(portfolio.shareCount(stockSymbol1), Eq(0u));
 }
 
-TEST_F(PortfolioTest, throwsWhenSellingMoreSharesThanPurchased) {
-    portfolio.purchase(stockSymbol1, 1u);
+TEST_F(PortfolioTest, throwsWhenSellingMoreSharesThanPurchaseHelperd) {
+    purchaseHelper(stockSymbol1, 1u);
     ASSERT_THROW(portfolio.sell(stockSymbol1, 2u), InvalidSellException);
 }
 
