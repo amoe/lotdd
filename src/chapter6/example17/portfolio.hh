969b5df9e0bd4bfd88bfab3f0528642894dd8d8f
David Banks <amoebae@gmail.com>
2023-02-23 08:17:27 +0000

    example16 -> example17

---
+++ src/chapter6/example17/portfolio.hh
@@ -0,0 +1,66 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <boost/date_time/gregorian/gregorian_types.hpp>
+
+class InvalidPurchaseException: public std::exception {
+};
+
+class ShareCountCannotBeZeroException: public std::exception {
+};
+
+class InsufficientSharesException: public std::exception {
+};
+
+
+struct PurchaseRecord {
+    PurchaseRecord(
+        int shareDelta, boost::gregorian::date date
+    ): shareDelta(shareDelta), date(date) {}
+    
+    int shareDelta;    // can be both positive and negative
+    boost::gregorian::date date;
+};
+
+
+class Portfolio {
+public:
+    Portfolio();
+
+    bool isEmpty() const;
+    
+    void purchase(
+        const std::string& symbol,
+        int shareCount,
+        const boost::gregorian::date& transactionDate
+    );
+    
+    void sell(
+        const std::string& symbol,
+        int shareCount,
+        const boost::gregorian::date& transactionDate
+    );
+
+    
+    int shareCount(const std::string& symbol) const;
+    std::vector<PurchaseRecord> purchases(const std::string& symbol) const;
+
+    static const boost::gregorian::date FIXED_PURCHASE_DATE;
+
+private:
+    void transact(
+        const std::string& symbol,
+        int shareCount,
+        const boost::gregorian::date& transactionDate
+    );
+
+    void throwIfShareCountIsZero(int shareChange) const;
+    void updateShareCount(const std::string&, int);
+    void addPurchaseRecord(const std::string&, int, const boost::gregorian::date&);
+
+    
+    int shareCount_;
+    std::unordered_map<std::string, int> shareHoldings;
+
+    std::unordered_map<std::string, std::vector<PurchaseRecord>> purchaseRecords;
+};
