e1fa0d24bc05108f28f6913b60d3b81094421422
David Banks <amoebae@gmail.com>
2023-12-03 19:59:06 +0000

    allow testserializable to compile

--- src/chapter7/example12/persistence_test.cc
+++ src/chapter7/example12/persistence_test.cc
@@ -1,15 +1,44 @@
+#include <string>
 #include <gmock/gmock.h>
 
+using std::string;
 using testing::Test;
 using testing::Eq;
 
+template <typename T>
+class Persistence {
+public:
+    virtual void add(T&) = 0;
+    virtual T get(const string& id) const = 0;
+};
+
+class Serializable {
+};
+
+class TestSerializable: public Serializable {
+public:
+    TestSerializable(const string& name, const string& id): name(name), id(id) { }
+
+private:
+    string name;
+    string id;
+};
+
 
 class PersistenceTest: public Test {
+public:
+    Persistence<TestSerializable>* persister;
+    TestSerializable* objectWithId1;
+
+    void SetUp() override {
+        objectWithId1 = new TestSerializable("one", "1");
+    }
 };
 
 // create 2 objects, 1 called persister
 
-
-TEST_P(PersistenceTest, addedItemCanBeRetrievedById) {
-    persister->add(*objectWithId1);
+// XXX: Should be using TEST_P macro
+TEST_F(PersistenceTest, addedItemCanBeRetrievedById) {
+//    persister->add(*objectWithId1);
+//    auto found = persister->get("1");
 }
