ee17a078e3b6ea3b7f75728354c85ce31bd3ba2a
David Banks <amoebae@gmail.com>
2024-01-31 07:14:11 +0000

    add initial writesSingleSample test

--- src/chapter8/example6/test_wav_reader.cc
+++ src/chapter8/example6/test_wav_reader.cc
@@ -1,14 +1,26 @@
+#include <sstream>
 #include <gmock/gmock.h>
 #include "wav_reader.hh"
 
+using std::ostringstream;
 using testing::Test;
 using testing::Eq;
 
-TEST(MyComponent, ActsAsIExpect) {
+class WavReaderTest: public Test {
+public:
+    ostringstream out;
+};
+
+TEST_F(WavReaderTest, writesSingleSample) {
+    char data[] { "abcd" };
     WavReader reader("foo", "bar");
 
-//    reader.open("foo", true);
-    
-    ASSERT_THAT(2 + 2, Eq(4));
+    uint32_t bytesPerSample{1};
+    uint32_t startingSample{0};
+    uint32_t samplesToWrite{1};
+
+    reader.writeSamples(&out, data, startingSample, samplesToWrite, bytesPerSample);
+
+    ASSERT_THAT("a", Eq(out.str()));
 }
 
