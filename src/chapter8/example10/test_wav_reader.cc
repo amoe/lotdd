7284205fef2a746863f0b45d7484ced523871cb0
David Banks <amoebae@gmail.com>
2024-02-06 06:38:46 +0000

    red test for wav reader

--- src/chapter8/example10/test_wav_reader.cc
+++ src/chapter8/example10/test_wav_reader.cc
@@ -35,3 +35,18 @@ TEST_F(WavReaderTest, writesMultibyteSampleFromMiddle) {
     ASSERT_THAT("89ABCD", Eq(out.str()));
 }
 
+TEST_F(WavReaderTest, incorporatesChannelCount) {
+    char data[] { "0123456789ABCDEFG" };
+
+    uint32_t bytesPerSample{2};
+    uint32_t startingSample{0};
+    uint32_t samplesToWrite{2};
+    uint32_t channels{2};
+
+    reader.writeSamples(
+        &out, data, startingSample, samplesToWrite, bytesPerSample,
+        channels
+    );
+    
+    ASSERT_THAT("01234567", Eq(out.str()));
+}
